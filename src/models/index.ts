96954985ff4aec6af615fb23350603d1e1a8d558
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-01 15:12:46 -0300

    add relation

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,9 +2,13 @@
 
 import { Category } from './Category'
 import { Course } from './Course'
+import { Episode } from './Episode'
 
 Category.hasMany(Course)
 
 Course.belongsTo(Category)
+Course.hasMany(Episode)
 
-export { Course, Category }
+Episode.belongsTo(Course)
+
+export { Category, Course, Episode }
