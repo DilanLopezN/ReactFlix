2496421a2279595c7dc0c3703443c6ebb5dbeb09
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-05 12:57:19 -0300

    api with useEffect

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { H1 } from '@adminjs/design-system'
+import { ApiClient } from 'adminjs'
 
 export default function Dashboard() {
+  const api = new ApiClient()
+
+  useEffect(() => {
+    fetchDashboardData()
+  }, [])
+
+  async function fetchDashboardData() {
+    const res = await api.getDashboard()
+    console.log(res.data)
+  }
+
   return (
     <section style={{ padding: '1.5rem' }}>
       <H1>Seja bem-vindo!</H1>
