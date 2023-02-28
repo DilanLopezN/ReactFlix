36e1f995dd6a5bcf57241a6790369c12cfe937a9
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-28 16:55:13 -0300

    categories pagination route

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -2,12 +2,33 @@ import { Request, Response } from 'express'
 import { Category } from '../models'
 export const categoriesController = {
   index: async (req: Request, res: Response) => {
+    const { page, perPage } = req.query
+
+    const perPageNumber =
+      typeof perPage === 'string' && parseInt(perPage, 10) > 0
+        ? parseInt(perPage, 10)
+        : 10
+
+    const pageNumber =
+      typeof page === 'string' && parseInt(page, 10) > 0
+        ? parseInt(page, 10)
+        : 1
+
+    const offset = (pageNumber - 1) * perPageNumber
+
     try {
-      const categories = await Category.findAll({
+      const { count, rows } = await Category.findAndCountAll({
         attributes: ['id', 'name', 'position'],
-        order: [['position', 'ASC']]
+        order: [['position', 'ASC']],
+        limit: perPageNumber,
+        offset
       })
-      return res.json(categories)
+      return res.json({
+        categories: rows,
+        page: pageNumber,
+        perPageNumber: perPageNumber,
+        total: count
+      })
     } catch (error) {
       console.error('Erro Categories Get', error)
     }
