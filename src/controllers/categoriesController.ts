44288f1167df3a80986c3e68c07b92b51daa3036
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-28 17:28:46 -0300

    get courses with id

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { getPaginatonParams } from '../helpers/getPaginationParams'
 import { categoryService } from '../services/categoryService'
 export const categoriesController = {
+  // GET /categories
   index: async (req: Request, res: Response) => {
     const [page, perPage] = getPaginatonParams(req.query)
     try {
@@ -13,6 +14,17 @@ export const categoriesController = {
       return res.json(paginatedCategories)
     } catch (error) {
       console.error('Erro Categories Get', error)
+    }
+  },
+
+  // GET /categories/:id
+  show: async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+      const category = await categoryService.findByIdWithCourses(id)
+      return res.json(category)
+    } catch (error) {
+      console.error('Erro Categories Get With ID', error)
     }
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
 const router = express.Router()
 
-router.get('/', categoriesController.index)
+router.get('/categories', categoriesController.index)
+router.get('/categories/:id', categoriesController.show)
 
 export { router }
