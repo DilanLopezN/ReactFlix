13b5f9fc78f935d8c634f62b55d7f74cbe62a2d1
Dilan Lopez <dilanlopez009@gmail.com>
2023-03-01 20:54:58 -0300

    new route courses

---
+++ src/controllers/coursesController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express'
+export const coursesController = {
+  // GET /courses/:id
+  show: async (req: Request, res: Response) => {
+    try {
+    } catch (error) {
+      console.error('Erro Courses Get With ID', error)
+    }
+  }
+}
