0ba6314d423e53578f376d8363bebd6da66c1254
Dilan Lopez <dilanlopez009@gmail.com>
2023-03-01 21:52:59 -0300

    featured courses endpoint

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from 'express'
 import { courseService } from '../services/coursesService'
 export const coursesController = {
+  // Get /courses/featured
+  featured: async (req: Request, res: Response) => {
+    try {
+    } catch (error) {
+      console.error('Erro Courses Get With ID', error)
+    }
+  },
   // GET /courses/:id
   show: async (req: Request, res: Response) => {
     const { id } = req.params
