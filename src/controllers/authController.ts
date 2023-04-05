311f6671139af3c0f13b8c48580f5b042768781f
Dilan Lopez <dilanlopez009@gmail.com>
2023-04-04 23:43:49 -0300

    login validate with jwt

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { userService } from "../services/userService";
 import { request } from "http";
+import { jwtService } from "../services/jwtService";
 
 export const authController = {
   register: async (req: Request, res: Response) => {
@@ -27,6 +28,37 @@ export const authController = {
         return res.status(400).json({message: error.message})
       }
       
+    }
+  },
+
+  // POST /auth/login
+
+  login: async (req: Request, res: Response) => {
+    const { email, password} = req.body
+
+    try {
+      const user = await userService.findByEmail(email)
+
+      if(!user) return res.status(404).json({message: 'Email não registrado'})
+
+      user.checkPassword(password, (err, isSame) => {
+        if(err) return res.status(400).json({message: err.message})
+
+        if(!isSame) return res.status(401).json({message: 'Senha incorreta'})
+
+        const payload = {
+          id: user.id,
+          firstName: user.firstName,
+          email: user.email
+        }
+        const token = jwtService.signToken(payload,'1d')
+
+        return res.json({ authenticaded: true, user, token})
+      })
+
+    } catch (error) {
+      return res.status(401).json({message: error})
+
     }
   }
 }
